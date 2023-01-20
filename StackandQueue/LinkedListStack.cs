0449fe81ef2c36691f7f80767f7ae4592756d163
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-20 14:32:11 +0530

    [Ashwani] - Created a stack

---
+++ StackandQueue/LinkedListStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackandQueue
+{
+    public class LinkedListStack
+    {
+        private Node top;
+
+        //Push the data in stack
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        //display the data of stack
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
---
+++ StackandQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StackandQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure using Stack And Queue");
+            Console.WriteLine("Create Stack using Linked List");
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
